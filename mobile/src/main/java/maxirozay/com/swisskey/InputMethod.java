d3c72081b45253d89b19847693add17bfc46a953
maxirozay <rossier.maxime@gmail.com>
2015-08-16 19:12:28 +0200

    capslock fixed

--- mobile/src/main/java/maxirozay/com/swisskey/InputMethod.java
+++ mobile/src/main/java/maxirozay/com/swisskey/InputMethod.java
@@ -8,7 +8,6 @@ import android.inputmethodservice.KeyboardView;
 import android.media.AudioManager;
 import android.os.Build;
 import android.text.InputType;
-import android.util.*;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
@@ -17,10 +16,6 @@ import android.widget.Button;
 
 import java.util.HashSet;
 import java.util.Set;
-
-import java.util.ArrayList;
-import java.util.List;
-
 
 /**
  * Created by Maxime Rossier on 13.08.2015.
@@ -41,12 +36,6 @@ public class InputMethod extends InputMethodService
     @Override
     public void onStartInput(EditorInfo attribute, boolean restarting) {
         super.onStartInput(attribute, restarting);
-        LoadDict dict= new LoadDict( getApplicationContext());
-        //dict.LoadFromFile("listeFR.txt");
-        //List<String> lstSms = new ArrayList<String>(dict.getAllSmsFromProvider());
-        List<SMSObject> lstSms = new ArrayList<SMSObject>(dict.readSMS());
-       // for(String element : lstSms)
-           // Log.d("contenuSMS",element);
         switch (attribute.inputType & InputType.TYPE_MASK_CLASS) {
             case InputType.TYPE_CLASS_NUMBER:
             case InputType.TYPE_CLASS_DATETIME:
@@ -182,6 +171,7 @@ public class InputMethod extends InputMethodService
                             setPrediction(lastWords[lastWords.length - 1], isFocused);
                         }
                     }
+
                 }
                 else {
                     ic.commitText(keyboard.getKeys().get(0).label.toString(), 1);
--- mobile/src/main/java/maxirozay/com/swisskey/Prediction.java
+++
@@ -1,7 +0,0 @@
-package maxirozay.com.swisskey;
-
-/**
- * Created by Leto on 16/08/2015.
- */
-public class Prediction {
-}
